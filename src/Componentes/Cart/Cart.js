24513ae0689eb440ab00b28327596c2138adcb08
Rodrigo Vargas <juanrodrigovv123@gmail.com>
2022-07-25 21:14:08 -0300

    Desafio Item Collection 2

--- src/Componentes/Cart/Cart.js
+++ src/Componentes/Cart/Cart.js
@@ -1,37 +1,184 @@
-import React, {useContext} from "react"
+import { Collections } from "@mui/icons-material";
+import React, {useContext, useState} from "react"
 import { Link } from "react-router-dom";
-import { contexto } from "../../Context/Contexto"   
+import { contexto } from "../../Context/Contexto";   
+import {collection, addDoc, serverTimestamp, doc, updateDoc} from "firebase/firestore";
+import { db } from '../../firebase/firebase';
+import Formulario from "../Formulario";
 
 
 
-const Cart = () => {
+const Cart = () => { 
+    const {limpiarCarro} = useContext(contexto);
 
 
+    const [idVenta, setidVenta] = useState("");
     const { productos, eliminarProductos, calcularTotal, calcularCantidad } = useContext(contexto);
+    const [nombre, setnombre] = useState("");
+    const [direccion, setdireccion] = useState("");
+    const [email, setemail] = useState("");
+    const [cPostal, setCpostal] = useState("");
+    const [datos, setdatos] = useState(false);
+
+
+
+
+    
+    const datosComprador = {
+        nombre: {nombre},
+        direccion: {direccion},
+        email: {email},
+        cPostal: {cPostal},
+    }
+
+    const finalizarCompra = () => {
+        const ventasCollection = collection(db,'ventas');
+            addDoc(ventasCollection, {
+                datosComprador,
+                items: productos,
+                total: calcularTotal(),               
+                date: serverTimestamp(),
+                
+            })
+            .then((result)=>{
+            setidVenta(result.id);
+            console.log(idVenta);            
+            alert('Su compra ha sido enviada, su número de consulta es ' + result.id)
+            
+            
+            })
+            limpiarCarro();
+
+    }
+
+   
+
+    productos.forEach(productos => {
+        const updateCollection = doc(db, "productos",productos.id);
+        updateDoc(updateCollection,{stock: productos.stock - productos.cantidad}); 
+        
+    });
+
 
     const remover = (data) => {
         eliminarProductos(data);
         console.log(data);
 
     }
+
+    const handleUser = (nombre,direccion,email,cpostal) => {
+        console.log(nombre,direccion,email,cpostal);
+        setnombre(nombre);
+        setdireccion(direccion);
+        setemail(email);
+        setCpostal(cpostal);
+        setdatos(true)
+    } 
+
+
+
     return (
+        
         <div>
-            {productos.length === 0 ? <Link to="/"><button> No hay productos. Comprar ahora</button></Link> :
+            
+            {productos.length === 0 ? <Link to="/"><button style={styles.button}> No hay productos. Comprar ahora</button></Link> :
             <>
             {productos.map(product => { return <>
-                <h2 key={product.id}>{product.title}"id"{product.id}"precio"{product.price}"cantidad"{product.cantidad}</h2>
-                <button onClick={() => remover(product)}>Remover producto</button>
+                <h2 style={styles.h2} key={product.id}>{product.title}{/* "id"{product.id} */}"precio"{product.price}"cantidad"{product.cantidad}</h2>
+                <img style={styles.imagen2}  src={product.image}alt=""  />
+                <button style={styles.button} onClick={() => remover(product)}>Remover producto</button>
        </>})}
-       <h1>   total $ {calcularTotal()}</h1>
-       <h1>   Cantidad de productos  {calcularCantidad()}</h1>
-
+       <h1 style={styles.h1}>   total $ {calcularTotal()}</h1>
+       <h1 style={styles.h1}>   Cantidad de productos  {calcularCantidad()}</h1>
+       <Formulario handleUser={handleUser} />
+        <button style={styles.button} disabled={datos == false}  onClick={finalizarCompra}>concretar compra</button>
        
        
       
             
            </>}
+        
+       
             
         </div>
+        
     )
+}
+const styles = {
+    button: {
+        
+        display: 'flex',
+        flexDirection: 'column',
+        aligContent: 'center',        
+        justifyItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        justifyText: 'center',
+        padding: '20px',
+        width: 300,
+
+        marginTop: '20px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        
+        
+
+    },
+    h1: {
+        
+        display: 'flex',
+        flexDirection: 'column',
+        aligContent: 'center',        
+        justifyItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        justifyText: 'center',
+        padding: '20px',
+        width: 500,
+
+        marginTop: '20px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        
+        
+
+    },
+    h2: {
+        
+        display: 'flex',
+        aligContent: 'center',        
+        justifyItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        justifyText: 'center',
+        padding: '20px',
+        width: '100%',
+
+        marginTop: '20px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        
+        
+
+    },
+    imagen2: {
+        
+        display: 'flex',
+        flexDirection: 'column',
+        aligContent: 'center',        
+        justifyItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        justifyText: 'center',
+        padding: '20px',
+        width: '100px',
+
+        marginTop: '20px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        
+        
+
+    },
 }
 export default Cart
---
+++ src/Componentes/Formulario.js
@@ -0,0 +1,98 @@
+import React, {useState} from "react";
+
+
+
+
+const Formulario = ({handleUser}) => {
+    
+
+    const [nombre, setNombre] = useState("");
+    const [direccion, setDireccion] = useState("");
+    const [email, setEmail] = useState("");
+    const [cpostal, setCpostal] = useState("");
+
+
+    return<>
+        
+        <form style={styles.formulario}>
+        <h1>Datos del comprador</h1>
+        <label style={styles.label} htmlFor="">Nombre</label>
+        <input style={styles.button} name="nombre" type="text" placeholder="Nombre" onChange={ev =>setNombre(ev.target.value)}></input>
+        <label style={styles.label} htmlFor="">Direccion</label>
+        <input style={styles.button} name="direccion" type="text" placeholder="Dirección" onChange={ev => setDireccion(ev.target.value)}></input>
+        <label style={styles.label} htmlFor="">Email</label>
+        <input style={styles.button} name="email" type="text" placeholder="Email" onChange={ev => setEmail(ev.target.value)}></input>
+        <label style={styles.label} htmlFor="">Código Postal</label>
+        <input style={styles.button} name="codigoPostal" type="number" placeholder="Código Postal" onChange={ev => setCpostal(ev.target.value)}></input>
+        
+        </form>
+        <button style={styles.button} onClick={() => handleUser(nombre,direccion,email,cpostal)}>Confirmar datos</button>
+    
+    </>
+
+}
+
+const styles = {
+  
+    formulario: {
+        
+        display: 'flex',
+        flexDirection: 'column',
+        aligContent: 'center',        
+        justifyItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        justifyText: 'center',
+        padding: '20px',
+        width: 1200,
+
+        marginTop: '20px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        
+        
+
+    },
+    button: {
+        
+        display: 'flex',
+        flexDirection: 'column',
+        aligContent: 'center',        
+        justifyItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        justifyText: 'center',
+        padding: '20px',
+        width: 300,
+
+        marginTop: '20px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        
+        
+
+    },
+    label: {
+        
+        display: 'flex',
+        flexDirection: 'column',
+        aligContent: 'center',        
+        justifyItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        justifyText: 'center',
+        padding: '20px',
+        width: 300,
+        fontSize: '25px',
+
+        marginTop: '20px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        
+        
+
+    },
+
+}
+
+export default Formulario
